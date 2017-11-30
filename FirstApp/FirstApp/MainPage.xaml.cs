dff975f751052510654baffdc97b5f070ac99c33
daryl sangalang <dee.yess@live.com>
2017-12-01 03:39:24 +1100

    First commit. followed youtube tutorial for a simple game

--- FirstApp/FirstApp/MainPage.xaml.cs
+++ FirstApp/FirstApp/MainPage.xaml.cs
@@ -1,17 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace FirstApp
 {
     public partial class MainPage : ContentPage
     {
+
+        static string bomb = new Random().Next(1,4).ToString();
+        private int score = 0;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        async void Button_Clicked(Object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            //Game Over
+            if (button.Text == bomb)
+            {
+                await DisplayAlert("Bomb Exploded!", "GAME OVER", "Retry");
+                bomb = new Random().Next(1, 4).ToString(); //assign new number as bomb
+                score = 0;
+            }
+            else
+            {
+                score++;
+                await DisplayAlert("Dodge!", "Score: " + score, "Continue");
+                bomb = new Random().Next(1,4).ToString();
+            }
         }
     }
 }
